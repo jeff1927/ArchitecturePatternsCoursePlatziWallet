8133fa550b63491ea8e6376bb7e9e3bd8db49f0b
Jefferson Roman <desarrollador.mobile@grupocarrera.com.co>
2023-05-27 17:43:09 -0500

    All MVP layers has been implemented

--- app/src/main/java/com/cristianvillamil/platziwallet/ui/home/view/FavoriteTransferAdapter.kt
+++ app/src/main/java/com/cristianvillamil/platziwallet/ui/home/view/FavoriteTransferAdapter.kt
@@ -1,9 +1,10 @@
-package com.cristianvillamil.platziwallet.ui.home
+package com.cristianvillamil.platziwallet.ui.home.view
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.cristianvillamil.platziwallet.R
+import com.cristianvillamil.platziwallet.ui.home.FavoriteTransfer
 
 class FavoriteTransferAdapter : RecyclerView.Adapter<FavoriteTransferViewHolder>() {
 
